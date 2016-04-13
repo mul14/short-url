e2d7b8a778b0e58a4ba861ad6b4131970440c0e0
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-04-13 17:34:28 +0700

    Add functionality

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,41 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $app->get('/', function () use ($app) {
-    return $app->version();
+    return response('Bad request', 400)->header('Content-type', 'text/plain');
+});
+
+$app->get('/{short_url}', function ($short_url) use ($app) {
+
+    $url = app('db')->table('urls')->where('short_url', $short_url)->first();
+
+    return $url
+        ? redirect($url->long_url)
+        : response('Bad request', 400)->header('Content-type', 'text/plain');
+
+});
+
+$app->post('/', function () use ($app) {
+
+    $url = app('db')->table('urls');
+
+    $longUrl = app('request')->input('url');
+
+    if (! $longUrl) {
+        return response('Bad request', 400)->header('Content-type', 'text/plain');
+    }
+
+    if ($data = $url->where('long_url', $longUrl)->first()) {
+        return response($data->short_url)->header('Content-type', 'text/plain');
+    }
+
+    do {
+        $shortUrl = str_random(5);
+    } while($url->where('short_url', $shortUrl)->first());
+
+    $url->insert([
+        'short_url' => $shortUrl,
+        'long_url' => $longUrl,
+    ]);
+
+    return response($shortUrl)->header('Content-type', 'text/plain');
 });
